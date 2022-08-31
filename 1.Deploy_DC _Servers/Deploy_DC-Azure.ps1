16687e3a93cce67a2b61e786840c1600dab73b03
amitaybo <46161569+amitaybo@users.noreply.github.com>
2022-08-31 22:33:55 +0300

    Add files via upload

---
+++ 1.Deploy_DC _Servers/Deploy_DC-Azure.ps1
@@ -0,0 +1,12 @@
+﻿#Install-Module -Name Az
+#Import-Module -Name Az
+
+$deploytemp = @{
+    adminUsername = 'amitay'
+    domainName = 'amicrosoftlab.com'
+    dnsPrefix = "adds01"   
+    
+}
+
+New-AzResourceGroup -Name myrg01 -Location 'East US'
+New-AzResourceGroupDeployment -ResourceGroupName myrg01 -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/application-workloads/active-directory/active-directory-new-domain-ha-2-dc/azuredeploy.json -TemplateParameterObject $deploytemp
